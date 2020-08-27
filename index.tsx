47b9623359b611933bc621c0d0efdf82cbbd473e
ircube <ivo753@gmail.com>
2020-08-27 08:36:06 -0400

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import SceneThree from './SceneThree';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <SceneThree />
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
